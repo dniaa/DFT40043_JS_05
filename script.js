84877d9358ded7a85f94088f55bf9830f1d54a13
dniaa <daniaariesya944@gmail.com>
2021-11-26 16:15:19 +0800

    done

---
+++ script.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    let nama = alert('Siapa nama?');
+    document.getElementById('tajuk').innerText = 'Selamat Datang ' + nama;
+}
+function simpan() {
+    let sahkan = confirm('Adakah anda pasti?');
+    if (sahkan) {
+        return true;
+    } else {
+        alert('Arahan simpan dibatalkan');
+        return false;
+    }
+}
+function fokus() {
+    document.getElementById('mesej').innerText = 'Sila klik butang Simpan';
+}
+function putih() {
+    document.getElementById('mesej').innerText = 'Sila nyatakan keperluan anda';
+}
